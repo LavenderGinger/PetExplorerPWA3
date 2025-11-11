f4a167450035580d51e3c4a97824e5d700cf060e
Lavender Ginger <a_guerragonzalez@mail.fhsu.edu>
2025-11-10 19:23:31 -0600

    this

--- serviceworker.js
+++ serviceworker.js
@@ -77,3 +77,19 @@ self.addEventListener('fetch', event => {
     })()
   );
 });
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request);
+    })
+  );
+});
