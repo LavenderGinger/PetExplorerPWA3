89cbad15e015439454d9deddb1963cdce12a55e3
Lavender Ginger <a_guerragonzalez@mail.fhsu.edu>
2025-11-10 20:41:43 -0600

    this

--- serviceworker.js
+++ serviceworker.js
@@ -77,3 +77,14 @@ self.addEventListener('fetch', event => {
     })()
   );
 });
+
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request);
+    })
+  );
+});
