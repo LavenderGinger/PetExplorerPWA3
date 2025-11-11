17dcedee66efee34808768735fcf58d23a7870cf
Lavender Ginger <a_guerragonzalez@mail.fhsu.edu>
2025-11-10 19:47:10 -0600

    this

--- src/indexedDB.js
+++ src/indexedDB.js
@@ -7,7 +7,7 @@ export async function initDB() {
   return openDB(DB_NAME, 1, {
     upgrade(db) {
       if (!db.objectStoreNames.contains(STORE_NAME)) {
-        const store = db.createObjectStore(STORE_NAME, { keyPath: "id" });
+        const store = db.createObjectStore(STORE_NAME, { keyPath: "id" , autoIncrement: true});
         store.createIndex("synced", "synced");
       }
     },
