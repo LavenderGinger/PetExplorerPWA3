067719924849ac68ce14adcd0d75b0abeff92271
Lavender Ginger <a_guerragonzalez@mail.fhsu.edu>
2025-11-10 21:44:04 -0600

    this

--- src/ui/app.js
+++ src/ui/app.js
@@ -18,7 +18,7 @@ export async function createRecord(data) {
   } else {
     notifyUser('Offline!');
     data.synced = false;
-    //data.id = 12345;
+    data.id = 'firebase-12345';
     await addIndexedDBRecord(data);
     notifyUser(`You are offline, record ${data} saved locally`);
   }
